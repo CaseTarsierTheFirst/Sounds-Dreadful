d18cf19273148b10518de966b22f34c9bde7e3c9
pen-bro <pbro3848@uni.sydney.edu.au>
2025-09-30 17:12:23 +1000

    Update bpm_estimator_top_level.sv

--- Audio_modules/bpm_hard_version_modules/bpm_estimator_top_level.sv
+++ Audio_modules/bpm_hard_version_modules/bpm_estimator_top_level.sv
@@ -1,7 +1,8 @@
-module bpm_estimator_top_level(
+
+module bpm_estimator_top_level #(
     parameter W = 16,
     parameter NSamples = 1024
-    )(
+)(
     //inputs
     input logic clk,
     input logic reset,
@@ -20,8 +21,10 @@ module bpm_estimator_top_level(
     //buffer N samples into RAM block (use ed lesson 4 1.3 FFT IP)
     //apply hanning window - multiply each smaple by precomputed coefficient from python code
 
+//FIFO - hanning wires
 logic [W-1:0] fft_input;
 logic fft_input_valid;
+logic fft_input_ready;
 logic frame_ready;
 
 fft_input_buffer #(.W(W), .NSamples(NSamples)) u_fifo_for_fft (
@@ -29,22 +32,29 @@ fft_input_buffer #(.W(W), .NSamples(NSamples)) u_fifo_for_fft (
     .reset(reset),
     .audio_clk(audio_clk),
     .audio_input_valid(audio_input_valid),
-    .audio_input_ready(audio_input_ready), //havent used yet
+    .audio_input_ready(), //havent used yet
     .audio_input_data(mic_stream),
+	 
     .fft_input(fft_input),
     .fft_input_valid(fft_input_valid),
+	 .fft_input_ready(fft_input_ready),
     .frame_ready(frame_ready)
 );
 
-logic [W+15:0] windowed_sample;
+//Hanning Window
+logic [W-1:0] windowed_sample;
+logic windowed_valid;
 logic [9:0] sample_index;
 
 hanning_window #(.W(W), .N(NSamples), .MAX_SAMPLE_INDEX(9)) u_hanning_window (
     .clk(clk),
     .reset(reset),
     .sample_in(fft_input),
+	 .sample_in_valid(fft_input_valid),
+	 .sample_in_ready(fft_input_ready),
     .sample_index(sample_index),
-    .windowed_sample(windowed_sample)
+    .windowed_sample(windowed_sample),
+	 .windowed_valid(windowed_valid)
 );
 
 //read values from FIFO and pass them through Hanning Window
@@ -52,46 +62,35 @@ always_ff @(posedge clk) begin
     if (reset) begin
         sample_index <= 0;
     end
-    else if (fft_input_valid) begin
-        sample_index <= sample_index + 1;
+    else if (fft_input_valid && fft_input_ready) begin
+        if(sample_index == NSamples-1) begin
+				sample_index <= 0;
+		  end
+		  else begin
+				sample_index <= sample_index + 1;
+		  end
     end
 end
 
 //compute magnitude spectrum on each frame as soon as it becomes available
     //use FFT IP core to compute magnitude spectrum
     //should pipeline this
+	 
+logic fft_valid;
+logic [15:0] fft_real_out;
+logic [15:0] fft_imag_out;
 
-logic [15:0] fft_sink_real, fft_sink_imag;
-logic fft_sink_valid, fft_sink_sop, fft_sink_eop;
-logic fft_sink_ready; //for backpressure
-
-logic [15:0] fft_out_real, fft_out_imag;
-logic fft_out_valid;
-
-assign fft_sink_real = windowed_sample[W+15:16]; //real part of input sample
-assign fft_sink_imag = 16'd0; //imaginary part - set to 0 fro real signals
-assign fft_sink_valid = fft_input_valid; //when input sample is valid
-assign fft_sink_sop = (sample_index == 0); //start of packet - assert on first sample of frame
-assign fft_sink_eop = (sample_index == NSamples-1); //end of packet - assert on last sample of frame
-
-//instantiating HARD IP FFT
-hard_ip_fft u_fft (
-    //Inputs to FFT
-    .clk(clk),
-    .reset_n(~reset),
-    .sink_valid(fft_sink_valid),
-    .sink_ready(fft_sink_ready),
-    .sink_sop(fft_sink_sop),
-    .sink_eop(fft_sink_eop),
-    .sink_real(fft_sink_real),
-    .sink_imag(fft_sink_imag),
-    .fftpts_in(NSamples),
-    //outputs from FFT
-    .source_valid(fft_out_valid),
-    .source_real(fft_out_real),
-    .source_imag(fft_out_imag),
-    .source_sop(),
-    .source_eop()
+FFT #(
+    .WIDTH(16)
+) fft_inst (
+    .clock(clk),
+    .reset(reset),
+    .di_en(windowed_valid),   // 1-cycle pulse when input is valid - input data enable
+    .di_re(windowed_sample),      // 16-bit real input from Hanning window
+    .di_im(16'd0),   // must use zeroes for imaginary part
+    .do_en(fft_valid),           // output valid pulse
+    .do_re(fft_real_out),        // real part of FFT output
+    .do_im(fft_imag_out)         // imaginary part of FFT output
 );
 
 logic [31:0] mag_sq;
@@ -101,9 +100,9 @@ fft_mag_sq #(.W(16)) u_mag_spec (
     //magnitude computation inputs
     .clk(clk),
     .reset(reset),
-    .fft_valid(fft_out_valid),
-    .fft_real(fft_out_real),
-    .fft_imag(fft_out_imag),
+    .fft_valid(fft_valid),
+    .fft_real(fft_real_out),
+    .fft_imag(fft_imag_out),
 
     //magnitude computation outputs
     .mag_sq(mag_sq),
@@ -167,7 +166,7 @@ spectral_flux #(.W(32), .N(1024)) u_spectral_flux (
 logic [15:0] bpm_low, bpm_mid, bpm_high;
 logic bpm_valid_low, bpm_valid_mid, bpm_valid_high;
 
-autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
+autocorrelation #(.W(32), .N(64), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
                 u_auto_low (
                     .clk(clk),
                     .reset(reset),
@@ -176,8 +175,9 @@ autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000
                     .BPM_estimate(bpm_low),
                     .bpm_valid(bpm_valid_low)
                 );
+					 
 
-autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
+autocorrelation #(.W(32), .N(64), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
                 u_auto_mid (
                      .clk(clk),
                     .reset(reset),
@@ -187,7 +187,7 @@ autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000
                     .bpm_valid(bpm_valid_mid)
                 );
 
-autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
+autocorrelation #(.W(32), .N(64), .SAMPLE_RATE(8000), .FRAME_SIZE(1024))
                 u_auto_high (
                      .clk(clk),
                     .reset(reset),
@@ -198,7 +198,7 @@ autocorrelation #(.W(32), .N(64), .MIN_LAG(20), .MAX_LAG(200), .SAMPLE_RATE(8000
                 );
 
 //period between beats is related to BPM via simple formula
-assign final_BPM_estimate = (2*bpm_low + bpm_mid + bpm_hight) / 4; //weight it to low bands - can tune and adjust this obviously
+assign final_BPM_estimate = (2*bpm_low + bpm_mid + bpm_high) / 4; //weight it to low bands - can tune and adjust this obviously
 //probably can even weight it towards something like the actual spectral flux magnitudes or like the confidence of the peaks idk - let's get it working first
     
 endmodule
